78c6fb4cb1620afff6031499bbe8aba5af89b00c
Sebastian Garcia <team.zylox1@gmail.com>
2026-06-02 23:17:45 -0600

    despliegue de astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cobach-ss.github.io',
+  base: '/Proyecto-gta-6',
   vite: {
     plugins: [tailwindcss()]
   }
